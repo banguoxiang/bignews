e9ff62886b62991f089e607c1d5a7838e72ece7d
banguoxiang <1786295283@qq.com>
2020-05-18 22:25:59 +0800

    完成了登录页面的功能并且添加了模态框效果

---
+++ admin/js/login.js
@@ -0,0 +1,33 @@
+$(function(){
+    $('.login_form').on('submit',function(e){
+        e.preventDefault()
+        $('.modal').modal('show')
+        $.ajax({
+            type:'post',
+            url:BigNew.user_login,
+            data:$(this).serialize(),
+            beforeSend:function(){
+                var flag=false
+                $('.login_form input[name]').each(function(index,ele){
+                    if($.trim($(ele).val())==''){
+                        flag = true
+                    }
+                })
+                if(flag){
+                    $('.modal-body p').text('输入内容不能为空！')
+                    return false
+                }
+            },
+            success:function(res){
+                $('.modal-body p').text(res.msg)
+                if(res.code==200){
+                    $('.modal').on('hidden.bs.modal', function (e) {
+                        window.localStorage.setItem(res.token)
+                        window.location.href = './index.html'
+
+                      })
+                }
+            }
+        })
+    })
+})
